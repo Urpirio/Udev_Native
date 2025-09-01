79b673dfcdaac4390d02ddbcaa0f6588ef26e4da
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-09-01 10:22:59 -0400

    Se agrego nuevo testing de las funcionalidades de la libreria

--- App-test/app/index.tsx
+++ App-test/app/index.tsx
@@ -1,10 +1,17 @@
 import { View, Text } from "react-native";
-import React from "react";
+import { useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Button, InputPassword } from "udev_ultime_native";
+import {
+  Button,
+  DropDown,
+  InputPassword,
+  InputText,
+  InputTextarea,
+} from "udev_ultime_native";
 
 export default function index() {
-  const [showPassword, setShowPassword] = React.useState(true);
+  const [showPassword, setShowPassword] = useState(true);
+  const [isOpen,setIsOpen] = useState(false);
 
   return (
     <SafeAreaProvider
@@ -51,6 +58,62 @@ export default function index() {
           ShowPassword={showPassword}
           iconPasswordHide={<Text>Show</Text>}
           iconPasswordShow={<Text>Hide</Text>}
+        />
+        <InputText
+          placeholder="Enter your text"
+          style_container={{
+            borderWidth: 1,
+            width: "100%",
+            borderColor: "gray",
+            borderRadius: 10,
+            padding: 10,
+          }}
+          style_input={{ width: "80%" }}
+          placeholderTextColor="gray"
+        />
+        <InputTextarea
+          placeholder="Enter your textarea"
+          style_container={{
+            borderWidth: 1,
+            width: "100%",
+            borderColor: "gray",
+            borderRadius: 10,
+            padding: 10,
+          }}
+          style_input={{ width: "80%" }}
+          placeholderTextColor="gray"
+        />
+
+        <DropDown
+          data_option={[
+            { label: "Option 1", value: 1 },
+            { label: "Option 2", value: 2 },
+            { label: "Option 3", value: 3 },
+          ]}
+          style_container={{
+            borderWidth: 1,
+            borderColor: "gray",
+            borderRadius: 10,
+            width: "100%",
+            alignItems:"center"
+          }}
+          placeholder="Select an option"
+          style_container_option={{
+            borderWidth: 1,
+            position: "absolute",
+            marginTop: 40,
+            width: "100%",
+            padding: 10,
+            borderRadius:10,
+            gap:10,
+          }}
+          style_text_placeholder={{ color: "gray",padding:10 }}
+          style_button_option={{
+            width: "100%",
+            borderWidth:1,
+            borderRadius:10,
+            padding:10,
+          }}
         />
       </View>
     </SafeAreaProvider>
