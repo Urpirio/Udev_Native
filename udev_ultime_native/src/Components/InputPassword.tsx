4d2a15420833329649716a71fca084e53448b204
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-09-02 16:16:51 -0400

    Añadido el componente InputPassword.
    Introduce un componente **InputPassword** reutilizable para React Native con funcionalidad de mostrar/ocultar contraseña y propiedades personalizables para estilo y accesibilidad.

---
+++ udev_ultime_native/src/Components/InputPassword.tsx
@@ -0,0 +1,94 @@
+import type { InputText_Props } from '../types';
+import { useState } from 'react';
+import { View, TextInput, TouchableOpacity } from 'react-native';
+
+export function InputPassword({
+  iconPasswordShow,
+  iconPasswordHide,
+  value,
+  onChangeText,
+  placeholder,
+  placeholderTextColor,
+  style_container,
+  style_input,
+  onAccessibilityAction,
+  onAccessibilityEscape,
+  onAccessibilityTap,
+  onBlur,
+  onChange,
+  onContentSizeChange,
+  onEndEditing,
+  onFocus,
+  onKeyPress,
+  onLayout,
+  onSelectionChange,
+  onMagicTap,
+  onPress,
+  onPressIn,
+  onPressOut,
+  onSubmitEditing,
+  readOnly,
+  clearTextOnFocus,
+  selectTextOnFocus,
+  showSoftInputOnFocus,
+  numberOfLines,
+}: InputText_Props) {
+  const [ShowPassword, setShowPassword] = useState<boolean>(false);
+  return (
+    <View
+      style={
+        style_container
+          ? style_container
+          : {
+              borderWidth: 1,
+              width: '100%',
+              borderColor: 'gray',
+              borderRadius: 10,
+              padding: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }
+      }
+    >
+      <TextInput
+        style={style_input ? style_input : { width: '80%' }}
+        secureTextEntry={ShowPassword}
+        value={value}
+        onChangeText={onChangeText}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onLayout={onLayout}
+        onKeyPress={onKeyPress}
+        onChange={onChange}
+        onContentSizeChange={onContentSizeChange}
+        onEndEditing={onEndEditing}
+        onSelectionChange={onSelectionChange}
+        onMagicTap={onMagicTap}
+        onAccessibilityAction={onAccessibilityAction}
+        onAccessibilityEscape={onAccessibilityEscape}
+        onAccessibilityTap={onAccessibilityTap}
+        onPress={onPress}
+        onPressIn={onPressIn}
+        onPressOut={onPressOut}
+        onSubmitEditing={onSubmitEditing}
+        readOnly={readOnly}
+        clearTextOnFocus={clearTextOnFocus}
+        selectTextOnFocus={selectTextOnFocus}
+        showSoftInputOnFocus={showSoftInputOnFocus}
+        numberOfLines={numberOfLines}
+        placeholder={placeholder}
+        placeholderTextColor={
+          placeholderTextColor ? placeholderTextColor : 'gray'
+        }
+      />
+      <TouchableOpacity
+        onPress={() =>
+          setShowPassword ? setShowPassword(!ShowPassword) : null
+        }
+      >
+        {ShowPassword ? iconPasswordHide : iconPasswordShow}
+      </TouchableOpacity>
+    </View>
+  );
+}
